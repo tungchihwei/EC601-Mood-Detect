c8c4ae12ea829ee6e72a0e4cb868789723568485
Chih Wei Tung <tu0803ng@gmail.com>
2017-11-20 10:26:43 -0500

    Update csv_col_combine.py

--- csv combine/csv_col_combine.py
+++ csv combine/csv_col_combine.py
@@ -1,7 +1,8 @@
 import csv
-file1=open("formants.csv")
-file2=open("pitch.csv")
-file3=csv.writer(open("Combine.csv","w"))
+file1=open("pitch.csv")
+file2=open("formants.csv")
+file3=open("MFCC.csv")
+file4=csv.writer(open("Combined.csv","w"))
 
 csv1=[]
 for line in file1:
@@ -15,6 +16,12 @@ for line in file2:
 	Str2=Str2.split(',')
 	csv2.append(Str2)
 
+csv3=[]
+for line in file3:
+	Str3=str(line.rstrip('\r\n'))
+	Str3=Str3.split(',')
+	csv3.append(Str3)
+
 for i in range(len(csv1)):
-	Str=csv1[i]+csv2[i]
-	file3.writerow(list(Str))
+	Str=csv1[i]+csv2[i]+csv3[i]
+	file4.writerow(list(Str))
