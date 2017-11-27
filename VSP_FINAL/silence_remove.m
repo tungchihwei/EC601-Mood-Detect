7ca21505bd65e72841910ab515fac2c97114d0b7
Zhonghao Guo <31747485+guozhonghao1994@users.noreply.github.com>
2017-11-26 21:11:45 -0500

    VSP_FINAL
    Nov.26

---
+++ VSP_FINAL/silence_remove.m
@@ -0,0 +1,36 @@
+%Remove the unvoiced parts/seperate to 2sec parts/save as *.wav file/Delete
+%the original record
+clear 
+clc
+folder = 'c:\sample\';
+files = dir([folder '*.wav']);
+N = 2; %Time length when dividing voice sample
+for i=1:length(files)
+    file = [folder files(i).name];
+    [Data,Fs] = detectVoiced(file);
+    for j=1:length(Data)
+        Value = cell2mat(Data(j));
+        if length(Value) <= Fs
+           continue
+        elseif length(Value) <= 2*Fs
+            wavwrite(Value,Fs,[folder num2str(i) '_' num2str(j) '.wav']);
+        else
+            num = floor(length(Value)/(N*Fs));
+            for k=1:num
+                Value_temp = Value((2*(k-1)*Fs+1):2*k*Fs,:);
+                wavwrite(Value_temp,Fs,[folder num2str(i) '_' num2str(j) '_' num2str(k) '.wav']);
+            end
+        end
+    end
+    delete(file);
+end
+
+    
+
+
+
+
+
+
+
+
